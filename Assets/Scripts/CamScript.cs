77f39940686b62ad31882431fe70edb1bc6d18c1
GuillaumePERRAY <bionikofficiel@gmail.com>
2018-01-15 20:30:33 +0100

    FirstCommit. Initial SetUp.

---
+++ Assets/Scripts/CamScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    public Transform camTarget;
+
+    public float camSpeed;
+
+    private float actualAngle;
+    private float _delay;
+
+    private bool _isRotating;
+
+    void Start () {
+
+        actualAngle = transform.rotation.eulerAngles.y;
+        transform.LookAt(camTarget);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_isRotating)
+        {
+            CameraRotation();
+            print("actualAngle: " + actualAngle);
+            print("Y: " + transform.rotation.eulerAngles.y);
+            _delay += Time.deltaTime;
+            if (_delay >= .5f)
+            {
+                print("ROTATE");
+                _isRotating = false;
+                _delay = 0;
+            }
+        }
+
+    }
+
+    public void CameraRotation ()
+    {
+        Vector3 targetPos = camTarget.transform.position;
+        transform.RotateAround(targetPos, new Vector3(0, 1, 0), -45 * Time.deltaTime * camSpeed);
+    }
+
+    public bool SetIsRotating (bool isRotating)
+    {
+        _isRotating = isRotating;
+        return isRotating;
+    }
+}
---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public CamScript scriptCamera;
+
+    //public bool _isRotating;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.Rotate(0, -45, 0);
+            //mainCamera.transform.position = Vector3.Lerp
+            scriptCamera.SetIsRotating(true);
+        }
+    }
+}
