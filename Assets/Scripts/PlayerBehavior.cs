e899b04bdf9f860ce67041d2fc71230355fb6c9b
GuillaumePERRAY <bionikofficiel@gmail.com>
2018-01-16 00:05:36 +0100

    Swipe Control; Jump; Crouch

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
+    public Swipe swipeControls;
     public float speed;
+    public float jumpforce;
 
+    private bool _isGrounded = true;
     // Use this for initialization
     void Start () {
 		
@@ -16,5 +19,32 @@ public class PlayerBehavior : MonoBehaviour {
 
         transform.Translate(-transform.forward * speed *Time.deltaTime);
 
-	}
+        if (swipeControls.SwipeUp && _isGrounded)
+        {
+            print("JUMP");
+            transform.Translate(0, 2 *jumpforce * Time.deltaTime ,-2 * speed * Time.deltaTime);
+            _isGrounded = false;
+        }
+
+        if (swipeControls.SwipeDown && _isGrounded)
+        {
+            print("Crouch");
+            transform.Translate(0, -2 * jumpforce * Time.deltaTime, -2 * speed * Time.deltaTime);
+            _isGrounded = false;
+        }
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="Ground")
+        {
+            _isGrounded = true;
+        }
+    }
 }
