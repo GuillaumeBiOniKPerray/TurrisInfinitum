aa9020dff760e53a7bacc883eccc1f551512cae6
Unknown <layecwilliam@gmail.com>
2018-01-17 18:04:41 +0100

    Début script sauvegarde

---
+++ Assets/Scripts/SwapManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapManager : MonoBehaviour {
+
+
+
+	public void BuyJaune()
+	{
+		PlayerPrefs.SetInt ("Jaune", 1);
+		print ("j'ai le skin jaune");
+	}
+
+	private void Start()
+	{
+		if (PlayerPrefs.GetInt ("Jaune") != 1) 
+		{
+			PlayerPrefs.SetInt ("Jaune", 0);
+			print ("pas le skin jaune");
+		} 
+		else 
+		{
+			print ("j'ai deja le skin jaune");
+		}
+	}
+}
