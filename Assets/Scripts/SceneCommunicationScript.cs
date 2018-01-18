74d7424f7b2899a1c037fe293733eb2f8535fdc5
Unknown <layecwilliam@gmail.com>
2018-01-18 20:14:34 +0100

    Mise en place de l'ui classe

--- Assets/Scripts/SceneCommunicationScript.cs
+++ Assets/Scripts/SceneCommunicationScript.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class SceneCommunicationScript : MonoBehaviour {
 
     public GameObject player;
+	public static GameObject intance;
+
 
     private void Start()
     {
-        DontDestroyOnLoad(gameObject); // Empeche ce script d'être détruit entre les scènes
-        //UpdatePlayerPrefab();
+		if (intance == null) 
+		{
+			intance = gameObject;
+			DontDestroyOnLoad(gameObject); // Empeche ce script d'être détruit entre les scènes
+			//UpdatePlayerPrefab();
+		}
+
     }
+		
 
     public void UpdatePlayerPrefab(GameObject newPlayerColor)
     {
         player = newPlayerColor;
     }
+		
 }
--- Assets/Scripts/SwapManager.cs
+++ Assets/Scripts/SwapManager.cs
@@ -7,13 +7,20 @@ public class SwapManager : MonoBehaviour {
     public SceneCommunicationScript menuInfo;
 
     private int actualMoney;
+	public GameObject yellowlocker;
+	public GameObject yellowunlocker;
+	public GameObject pinklocker;
+	public GameObject pinkunlocker;
+	public GameObject menuPerso;
+	public Material mat_Pink;
+	public Material mat_Yellow;
     //public GameObject player;
 
     private void Start()
     {
         //PlayerPrefs.DeleteKey("Jaune");
         //PlayerPrefs.DeleteKey("Pink");
-        //PlayerPrefs.SetInt("Money", 30);
+        //PlayerPrefs.SetInt("Money", 60);
         actualMoney = PlayerPrefs.GetInt("Money");
 
         if (PlayerPrefs.GetInt("Jaune") != 1)
@@ -35,6 +42,9 @@ public class SwapManager : MonoBehaviour {
             actualMoney -= 30;
             PlayerPrefs.SetInt("Money", actualMoney);
             print("j'ai le skin jaune");
+			yellowlocker.SetActive(false);
+			yellowunlocker.SetActive (true);
+
             //menuInfo.UpdatePlayerPrefab(yellowPlayer);
         }
         else if (PlayerPrefs.GetInt("Jaune") == 0 && PlayerPrefs.GetInt("Money") < 30)
@@ -44,6 +54,10 @@ public class SwapManager : MonoBehaviour {
 
         if(PlayerPrefs.GetInt("Jaune") == 1)
         {
+			yellowlocker.SetActive(false);
+			yellowunlocker.SetActive (true);
+
+			menuPerso.GetComponent<Renderer>().material = mat_Yellow;
             print("changement de skin pour " + yellowPlayer);
             //player = yellowPlayer;
             menuInfo.UpdatePlayerPrefab(yellowPlayer);
@@ -59,6 +73,8 @@ public class SwapManager : MonoBehaviour {
             actualMoney -= 30;
             PlayerPrefs.SetInt("Money", actualMoney);
             print("j'ai le skin rose");
+			pinklocker.SetActive(false);
+			pinkunlocker.SetActive (true);
         }
         else if (PlayerPrefs.GetInt("Pink") == 0 && PlayerPrefs.GetInt("Money") < 30)
         {
@@ -68,8 +84,11 @@ public class SwapManager : MonoBehaviour {
         if (PlayerPrefs.GetInt("Pink") == 1)
         {
             //player = yellowPlayer;
-            menuInfo.UpdatePlayerPrefab(pinkPlayer);
+			menuPerso.GetComponent<Renderer>().material = mat_Pink;
+			menuInfo.UpdatePlayerPrefab(pinkPlayer);
             print("changement de skin pour " + pinkPlayer);
+			pinklocker.SetActive(false);
+			pinkunlocker.SetActive (true);
         }
 
     }
