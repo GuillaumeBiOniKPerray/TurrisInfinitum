95ed5395fe37863dd2d574bf650133a515dcef29
Unknown <layecwilliam@gmail.com>
2018-01-18 15:12:09 +0100

    Ajouts de 6 levels + rework des anciens

---
+++ Assets/Scripts/Coins_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins_Script : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider player)
+	{
+		if (player.gameObject.tag == "Player") 
+		{
+			int i = PlayerPrefs.GetInt ("Money");
+			i++;
+			PlayerPrefs.SetInt ("Money", i);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/CustomPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlayer : MonoBehaviour {
+
+	public Swipe swipeControls;
+
+	public float speed;
+	public float jumpForce;
+	public float jumpLenght;
+
+	private bool _isGrounded = true;
+
+	void Update () 
+	{
+
+			transform.Translate(transform.InverseTransformDirection(transform.forward) * speed * Time.deltaTime);
+			Jump();
+			/*if (swipeControls.SwipeDown && _isGrounded)
+            {
+                transform.Translate(0, -2 * jumpforce * Time.deltaTime, -2 * speed * Time.deltaTime);
+                _isGrounded = false;
+            }*/
+
+	}
+
+	void Jump() // Fonction de saut régler sur le swipe vers le haut (ou click and drag avec la souris)
+	{
+		if (swipeControls.SwipeUp && _isGrounded)
+		{
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, -jumpLenght), ForceMode.Impulse);
+			_isGrounded = false;
+			GetComponent<Rigidbody>().useGravity = true;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision) //Fonction vérifiant que le joueur soit sur le sol (empêche le double saut)
+	{
+		if(collision.gameObject.tag =="Ground")
+		{
+			_isGrounded = true;
+		}
+	}
+}
